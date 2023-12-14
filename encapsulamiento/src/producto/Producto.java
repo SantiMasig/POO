800675963bf65e471dd82b61ba9dfdc3f407b94d
SantiMasig <santimasig@outlook.com.ar>
2023-12-14 18:09:00 -0300

    Producto-Encapsulamiento

---
+++ encapsulamiento/src/producto/Producto.java
@@ -0,0 +1,60 @@
+package producto;
+
+public class Producto {
+
+	private int codigo;
+	private String descripcion;
+	private float precioUnitario;
+	private int stock;
+
+	public Producto() {
+
+	}
+
+	public Producto(int codigo, String descripcion, float precioUnitario, int stock) {
+		super();
+		this.codigo = codigo;
+		this.descripcion = descripcion;
+		this.precioUnitario = precioUnitario;
+		this.stock = stock;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public float getPrecioUnitario() {
+		return precioUnitario;
+	}
+
+	public void setPrecioUnitario(float precioUnitario) {
+		this.precioUnitario = precioUnitario;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	@Override
+	public String toString() {
+		return "Producto [codigo=" + codigo + ", descripcion=" + descripcion + ", precioUnitario=" + precioUnitario
+				+ ", stock=" + stock + "]";
+	}
+
+}
